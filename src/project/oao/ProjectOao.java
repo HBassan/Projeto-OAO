b87dff716d37b97a4e34686752deaf27c65bafd2
HBassan <h53460272@gmail.com>
2024-11-09 00:55:56 -0300

    Starting Project

---
+++ src/project/oao/ProjectOao.java
@@ -0,0 +1,14 @@
+package project.oao;
+
+/**
+ *
+ * @author Henrique Bassan
+ */
+public class ProjectOao {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
